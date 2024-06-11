010f5e8dbf74a58b9716d8ed0fc0ca667d59daea
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-06-11 16:56:35 +0200

    Get exchange rate from API

--- js/main.js
+++ js/main.js
@@ -44,9 +44,9 @@ $(document).ready(function() {
 
     // Function to calculate the price based on selected options
     function calcPrice() {
-        var modelPriceMotor = $('input[name=engine]:checked', '#autoForm').val();
-        var modelPriceTrans = $('input[name=transmission]:checked', '#autoForm').val();
-        var modelPricePackage = $('input[name=package]:checked', '#autoForm').val();
+        let modelPriceMotor = $('input[name=engine]:checked', '#autoForm').val();
+        let modelPriceTrans = $('input[name=transmission]:checked', '#autoForm').val();
+        let modelPricePackage = $('input[name=package]:checked', '#autoForm').val();
 
         modelPriceMotor = parseInt(modelPriceMotor) || 0;
         modelPriceTrans = parseInt(modelPriceTrans) || 0;
@@ -58,9 +58,9 @@ $(document).ready(function() {
     
     // Funktion to complete specifications based on selected options
     function completeSpecs() {
-        var motorText = $('input[name=engine]:checked + label', '#autoForm').text() || '';
-        var transText = $('input[name=transmission]:checked + label', '#autoForm').text() || '';
-        var packageText = $('input[name=package]:checked + label', '#autoForm').text() || '';
+        let motorText = $('input[name=engine]:checked + label', '#autoForm').text() || '';
+        let transText = $('input[name=transmission]:checked + label', '#autoForm').text() || '';
+        let packageText = $('input[name=package]:checked + label', '#autoForm').text() || '';
 
         modelSpecs = motorText;
         modelSpecs += motorText && transText ? ', ' + transText : transText;
@@ -71,15 +71,28 @@ $(document).ready(function() {
     // Function to add spaces between each thousands
     function addSpace(nStr) {
         nStr += '';
-        var x = nStr.split('.');
-        var x1 = x[0];
-        var x2 = x.length > 1 ? '.' + x[1] : '';
-        var rgx = /(\d+)(\d{3})/;
+        let x = nStr.split('.');
+        let x1 = x[0];
+        let x2 = x.length > 1 ? '.' + x[1] : '';
+        let rgx = /(\d+)(\d{3})/;
         while (rgx.test(x1)) {
         x1 = x1.replace(rgx, '$1' + ' ' + '$2');
         }
         return x1 + x2;
     }
 
+    // API key and URL to retrieve exchange rate
+    let currencyUrl = 'https://v6.exchangerate-api.com/v6/47620c96b5dc53f3680ed238/latest/USD';
+    var sekUsdRate = 0;
+
+    // Get exchange rate from API
+    $.ajax({
+        url: currencyUrl,
+        cache: false,
+        success: function(response) {
+        sekUsdRate = response.conversion_rates.SEK;
+        calcUSD();
+        }
+    });
 });
   
