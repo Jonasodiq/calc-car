8372f61b7ef890011ae82411e378790081fdde1a
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-06-11 16:45:32 +0200

    Add Variables

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//link- https://api.jquery.com/category/traversing/filtering/
+//Tree Traversal https://api.jquery.com/category/traversing/tree-traversal/
+
+// Calc-Car
+
+$(document).ready(function() {
+    // Variables
+    let modelSpecs,
+        modelPrice,
+        modelSpecsHolder,
+        modelPriceHolder,
+        modelPriceUSDHolder;
+  
+    
+});
+  
