a7b39e483ce2e2c63def4bfca78725d0cd80fc04
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-06-11 16:54:42 +0200

    Add Function to add spaces between each thousands

--- js/main.js
+++ js/main.js
@@ -67,6 +67,19 @@ $(document).ready(function() {
         modelSpecs += motorText || transText ? ' ' + packageText : packageText;
         modelSpecsHolder.text(modelSpecs);
     }
-    
+
+    // Function to add spaces between each thousands
+    function addSpace(nStr) {
+        nStr += '';
+        var x = nStr.split('.');
+        var x1 = x[0];
+        var x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ' ' + '$2');
+        }
+        return x1 + x2;
+    }
+
 });
   
