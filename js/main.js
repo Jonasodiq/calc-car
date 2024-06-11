903afc5a12270d1ce98a2db194dd89f112d594b5
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-06-11 16:52:04 +0200

    Add funcion to calc the price and click alert

--- js/main.js
+++ js/main.js
@@ -18,6 +18,43 @@ $(document).ready(function() {
 
     modelSpecs = '';
     modelPrice = 0;
+
+    // Changes in the form and update price and specifications
+    $('#autoForm input').on('change', function() {
+        completeSpecs();
+        calcPrice();
+        calcUSD();
+    });
+
+    // Call to display default values
+    completeSpecs();
+    calcPrice();
+    calcUSD();
+
+    // Click on color selection and update image
+    $('#colorsSelector .colorItem').on('click', function() {
+        var imgPath = $(this).attr('data-img-path');
+        $('#imgHolder img').attr('src', imgPath);
+    });
+
+    // Click button and call alert
+    $( "#button" ).on( "click", function() {
+        alert( "Your order is purchase successful." );
+    } );
+
+    // Function to calculate the price based on selected options
+    function calcPrice() {
+        var modelPriceMotor = $('input[name=engine]:checked', '#autoForm').val();
+        var modelPriceTrans = $('input[name=transmission]:checked', '#autoForm').val();
+        var modelPricePackage = $('input[name=package]:checked', '#autoForm').val();
+
+        modelPriceMotor = parseInt(modelPriceMotor) || 0;
+        modelPriceTrans = parseInt(modelPriceTrans) || 0;
+        modelPricePackage = parseInt(modelPricePackage) || 0;
+
+        modelPrice = modelPriceMotor + modelPriceTrans + modelPricePackage;
+        modelPriceHolder.text('SEK: ' + addSpace(modelPrice)); 
+    }
     
 });
   
